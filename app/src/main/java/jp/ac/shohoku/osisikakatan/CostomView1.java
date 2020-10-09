e56f3a91a79e242297833f870ee5f1cadbaaaaa2
s19B712 <s19b712@shohokuac.jp>
2020-10-09 09:59:36 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/shohoku/osisikakatan/CostomView1.java
@@ -0,0 +1,17 @@
+package jp.ac.shohoku.osisikakatan;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CostomView1 extends View {
+    public CostomView1(Context context) {
+        super(context);
+    }
+
+    public CostomView1(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
---
+++ app/src/main/java/jp/ac/shohoku/osisikakatan/CostomView2.java
@@ -0,0 +1,17 @@
+package jp.ac.shohoku.osisikakatan;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CostomView2 extends View {
+    public CostomView2(Context context) {
+        super(context);
+    }
+
+    public CostomView2(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
